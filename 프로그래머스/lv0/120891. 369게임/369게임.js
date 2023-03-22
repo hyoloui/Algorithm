8131aaf2d22bc42a2750ad330140ad4dbe5a330d
이승효 <115724947+hyoloui@users.noreply.github.com>
2023-03-22 12:33:57 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,6 +1,6 @@
 function solution(order) {
-    // 필터로 length 조지기
-    const stringy = [...order.toString()]
-    const filtering = stringy.filter((num) => num == 3 || num == 6 || num == 9)
-    return filtering.length
+    return [...order.toString()].filter((num) => {
+        if(num == 0) return false;
+        return num % 3 == 0        
+    }).length
 }
