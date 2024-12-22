94734930938d54a20ac46e348515eace3848fe87
hyoloui <115724947+hyoloui@users.noreply.github.com>
2024-12-23 00:05:14 +0900

    [level 0] Title: 5명씩, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.py
+++ 프로그래머스/0/181886. 5명씩/5명씩.py
@@ -1,8 +1,2 @@
 def solution(names):
-    answer = []
-    for n in enumerate(names):
-        idx = n[0]
-        name = n[1]
-        if (idx % 5 == 0):
-            answer.append(name)
-    return answer
+    return names[::5]
