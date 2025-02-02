d04f1dbbe84db0968e2c5b1c3f1fb8d90ae08f3e
hyoloui <115724947+hyoloui@users.noreply.github.com>
2025-02-02 09:45:25 +0900

    [level 0] Title: 공배수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.py
+++ 프로그래머스/0/181936. 공배수/공배수.py
@@ -1,6 +1,6 @@
 def solution(number, n, m):
-    case_1 = number / n
-    case_2 = number / m
-    if case_1.is_integer() and case_2.is_integer():
-        return 1
+    c1 = number / n
+    c2 = number / m
+
+    if c1.is_integer() and c2.is_integer(): return 1
     else : return 0
