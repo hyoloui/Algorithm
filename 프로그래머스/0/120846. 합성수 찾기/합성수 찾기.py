db50aef3c6cfa5485096529f9f4fb2c9db33b12a
hyoloui <115724947+hyoloui@users.noreply.github.com>
2025-02-02 10:48:41 +0900

    [level 0] Title: 합성수 찾기, Time: 0.44 ms, Memory: 9.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(4, n + 1):
+        count = 0
+        for x in range(1, i + 1):
+            if i % x == 0:
+                count += 1
+        if count >= 3:
+            answer += 1
+            
+    return answer
