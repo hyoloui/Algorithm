9d6c89b24ac47ac3cd300c3aa0b478d3885a132b
hyoloui <115724947+hyoloui@users.noreply.github.com>
2025-02-02 09:59:53 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -1,12 +1,11 @@
 def solution(n):
     answer = 0
     
-    for i in range(n+1):
-        if isEven(n) :
-            if isEven(i): answer += i ** 2
-        else :
-            if not isEven(i) : answer += i
+    if n % 2:
+        for i in range(1, n+1, 2):
+            answer += i
+    else :
+        for i in range(2, n+1, 2):
+            answer += i * i
         
     return answer
-
-def isEven(n): return n % 2 == 0
