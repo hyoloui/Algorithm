700cad21bb35efa0bec968da1aac2257dff5cb8a
hyoloui <115724947+hyoloui@users.noreply.github.com>
2025-02-04 08:37:29 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,7 +1,5 @@
 def solution(arr, queries):
     for a, b in queries:
-        copy_a = arr[a]
-        arr[a] = arr[b]
-        arr[b] = copy_a
+        arr[a],arr[b]=arr[b],arr[a]
 
     return arr
