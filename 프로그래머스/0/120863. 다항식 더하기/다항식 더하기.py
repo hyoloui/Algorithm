e93c40e76049e11e139f9523b71884c790f0ef79
hyoloui <115724947+hyoloui@users.noreply.github.com>
2025-04-30 23:21:40 +0900

    [level 0] Title: 다항식 더하기, Time: 0.03 ms, Memory: 9.53 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,21 @@
+def solution(polynomial):
+    answer = []
+    xNum = 0
+    num = 0
+    
+    arr = polynomial.split(" + ")
+    numFirst = arr[0].isdigit()
+    for n in arr:
+        if n.endswith('x'):
+            if n[0] == 'x': xNum += 1
+            else: xNum += int(n[:-1])
+        else:
+            num += int(n)
+            
+    if xNum:
+        if xNum == 1: answer.append('x')
+        else: answer.append(f'{xNum}x')
+    if xNum and num: answer.append(' + ')
+    if num: answer.append(str(num))
+    
+    return ''.join(answer)
