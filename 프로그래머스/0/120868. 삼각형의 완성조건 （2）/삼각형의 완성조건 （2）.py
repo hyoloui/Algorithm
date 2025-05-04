329e993a662e64cdf184df0f37c999930aa1f85a
hyoloui <115724947+hyoloui@users.noreply.github.com>
2025-05-04 13:49:14 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.36 ms, Memory: 9.37 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -0,0 +1,13 @@
+def solution(sides):
+    answer = 0
+    max_side = max(sides)
+    min_side = min(sides)
+    
+    for new_side in range(max_side - min_side + 1, max_side + 1):
+        print(new_side)
+        answer += 1
+        
+    for new_side in range(max_side + 1, max_side + min_side):
+        answer += 1
+        
+    return answer
