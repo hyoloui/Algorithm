90687ed576323414f0c787ade680320805ffebbf
hyoloui <115724947+hyoloui@users.noreply.github.com>
2025-05-10 14:45:53 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -1,10 +1,13 @@
 def solution(chicken):
-    total_service_chickens = 0  # 총 서비스 치킨 수
-    coupons = chicken           # 현재 보유 쿠폰 수 (처음 시켜먹은 치킨 수만큼 쿠폰 발급)
-    while coupons >= 10:
-        new_service_chickens = coupons // 10
-        total_service_chickens += new_service_chickens
-        remaining_coupons = coupons % 10
-        coupons = new_service_chickens + remaining_coupons
+    return int(chicken*0.11111111111)
+    
+#     total_service_chickens = 0  # 총 서비스 치킨 수
+#     coupons = chicken           # 현재 보유 쿠폰 수 (처음 시켜먹은 치킨 수만큼 쿠폰 발급)
+#     while coupons >= 10:
+#         new_service_chickens = coupons // 10
+#         total_service_chickens += new_service_chickens
+#         remaining_coupons = coupons % 10
+#         coupons = new_service_chickens + remaining_coupons
         
-    return total_service_chickens
+
+#     return total_service_chickens
