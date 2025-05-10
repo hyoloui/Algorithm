caa0e3d555e598cb48347f93db59bb88d6c1f570
hyoloui <115724947+hyoloui@users.noreply.github.com>
2025-05-10 14:17:47 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,12 @@
+def solution(id_pw, db):
+    input_id, input_pw = id_pw
+    answer = 'fail'
+
+    for user_id, user_pw in db:
+        if input_id == user_id:
+            if input_pw == user_pw:
+                return 'login' 
+            else:
+                return 'wrong pw'
+        
+    return answer
